901c73cec89b965ccd2c50c3fd32c90b5714dd53
Ibrahim Awny <67521813+hima12-awny@users.noreply.github.com>
2023-07-01 21:43:49 +0300

    Add files via upload

---
+++ NewsGUI/loginpage.cpp
@@ -0,0 +1,150 @@
+
+#include "loginpage.h"
+#include "registerpage.h"
+
+LoginPage::LoginPage(
+    loadAndStoreFiles* tools, 
+    QWidget*           parent) 
+
+    :QWidget{parent}, 
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    tool = tools;
+    tool->loadNews();
+    tool->loadUsers();
+    
+    setGeometry(0, 0, 1371, 932);
+    setFixedSize(size());
+    setWindowTitle("Login");
+
+    QLabel* imageLabel = new QLabel(this);
+    QPixmap image("K:\\C++\\GUI Qt\\NewsGUI\\imgs\\User login.png");
+
+    QPixmap scaledImage = image.scaled(size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    imageLabel->setPixmap(scaledImage);
+    imageLabel->move(0, 0);
+
+    QPushButton*  RegBtn = new QPushButton("Register",this);
+    RegBtn->setGeometry(696,644,269,50);
+    RegBtn->setStyleSheet("QPushButton {\
+                            font-size:20px;\
+                            font-family:Inter;\
+                            border-radius: 20px;\
+                            background-color: #D9D9D9;\
+                            padding:10px;\
+                          }\
+                          QPushButton:hover {\
+                            background-color: #245B96;\
+                            color:#fff;\
+                            transition: background-color 16s ease-in-out;\
+                          }");
+    connect(RegBtn, &QPushButton::clicked, this, &LoginPage::registerUser);
+
+    QPushButton*  LoginBtn = new QPushButton("Login",this);
+    LoginBtn->setGeometry(392,644,269,50); // use LoginBtn instead of RegBtn here
+    LoginBtn->setStyleSheet("QPushButton {\
+                            font-size:20px;\
+                            font-family:Inter;\
+                            border-radius: 20px;\
+                            background-color: #D9D9D9;\
+                            padding:10px;\
+                          }\
+                          QPushButton:hover {\
+                            background-color: #245B96;\
+                            color:#fff;\
+                            transition: background-color 16s ease-in-out;\
+                          }");
+
+    connect(LoginBtn, &QPushButton::clicked, this, &LoginPage::login);
+
+
+    // Set up username line edit
+    username = new QLineEdit(this);
+    username->setGeometry(583, 470, 382, 50);
+    username->setStyleSheet("QLineEdit {\
+                                  font-size: 20px;\
+                                  font-family: Inter;\
+                                  border-radius: 20px;\
+                                  background-color: #D9D9D9;\
+                                  padding: 10px;\
+                                }");
+    username->setFocus();
+
+    // Set up password line edit
+    password = new QLineEdit(this);
+    password->setEchoMode(QLineEdit::Password);
+    password->setGeometry(583, 550, 382, 50);
+    password->setStyleSheet(
+        "QLineEdit {\
+            font-size: 20px;\
+            font-family: Inter;\
+            border-radius: 20px;\
+            background-color: #D9D9D9;\
+            padding: 10px;\
+        }");
+
+    QPushButton* exitBtn = new QPushButton("Exit", this);
+    exitBtn->setGeometry(25, 847, 269, 50); // use LoginBtn instead of RegBtn here
+    exitBtn->setStyleSheet("QPushButton {\
+                                font-size:20px;\
+                                font-family:Inter;\
+                                border-radius: 20px;\
+                                background-color: #D9D9D9;\
+                                padding:10px;\
+                              }\
+                          QPushButton:hover {\
+                            background-color: #245B96;\
+                            color:#fff;\
+                            transition: background-color 16s ease-in-out;\
+                          }");
+
+    connect(exitBtn, &QPushButton::clicked, this, &LoginPage::savaAndClose);
+}
+
+void LoginPage::savaAndClose() {
+
+    tool->saveAndClose(tool->users, tool->news);
+    this->close();
+
+}
+
+void LoginPage::registerUser()
+{
+    RegisterPage *newWidget = new RegisterPage(tool,this);
+    newWidget->show();
+}
+
+void LoginPage::login() {
+
+    string usernameSTD = username->text().toUtf8().constData();
+    string passwordSTD = password->text().toUtf8().constData();
+
+    if (usernameSTD.empty() || passwordSTD.empty()) {
+        QMessageBox::critical(this, "Error", "enter the required credentials.");
+    }
+    else if (tool->users[usernameSTD].username.empty()) {
+
+        QMessageBox::warning(this, "Error", "User Doesn't Exist");
+    }
+    else if (tool->users[usernameSTD].pass != passwordSTD) {
+        QMessageBox::warning(this, "Error", "Incorrect Password.");
+    }
+    else {
+
+        if (tool->users[usernameSTD].role == "user") {
+            user = new UserWidget(username->text(), tool, this);
+            user->show();
+        }
+        else {
+            admin = new adminWidget(username->text(), tool, this);
+            admin->show();
+        }
+    }
+}
+
+LoginPage::~LoginPage()
+{
+    delete ui;
+}
---
+++ NewsGUI/out/build/x64-Debug/CMakeFiles/d/62b02d7bf583048a52cb27b786ae1d94652e5fdf319624eb6e3b4dddb1ffbb7d.d
@@ -0,0 +1,205 @@
+NewsGUI_autogen/timestamp: \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/CMakeCXXInformation.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/CMakeCommonLanguageInclude.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/CMakeGenericSystem.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/CMakeInitializeConfigs.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/CMakeLanguageInformation.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/CMakeParseArguments.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/CMakeRCInformation.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/CMakeSystemSpecificInformation.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/CMakeSystemSpecificInitialize.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/Compiler/CMakeCommonCompilerMacros.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/Compiler/MSVC-CXX.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/Compiler/MSVC.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/Platform/Windows-MSVC-CXX.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/Platform/Windows-MSVC.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/Platform/Windows.cmake \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.26/Modules/Platform/WindowsPaths.cmake \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/QString \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qabstractitemmodel.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qalgorithms.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qarraydata.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qatomic.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qbasicatomic.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qbytearray.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qbytearraylist.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qchar.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qcompilerdetection.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qconfig.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qcontainerfwd.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qcontainertools_impl.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qdatastream.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qflags.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qglobal.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qglobalstatic.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qhash.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qhashfunctions.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qiodevice.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qiterator.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qline.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qlist.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qlocale.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qlogging.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qmap.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qmargins.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qmetatype.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qmutex.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qnamespace.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qnumeric.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qobject.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qobject_impl.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qobjectdefs.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qobjectdefs_impl.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qpair.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qpoint.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qprocessordetection.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qrect.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qrefcount.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qregexp.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qregularexpression.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qscopedpointer.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qshareddata.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qsharedpointer.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qsharedpointer_impl.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qsize.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qstring.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qstringalgorithms.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qstringlist.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qstringliteral.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qstringmatcher.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qstringview.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qsysinfo.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qsystemdetection.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qtcore-config.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qtypeinfo.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qurl.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qvariant.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qvarlengtharray.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qvector.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtCore/qversiontagging.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qbrush.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qcolor.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qcursor.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qfont.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qfontinfo.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qfontmetrics.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qicon.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qimage.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qkeysequence.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qmatrix.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qpaintdevice.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qpalette.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qpen.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qpixelformat.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qpixmap.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qpolygon.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qregion.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qrgb.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qrgba64.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qtextcursor.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qtextdocument.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qtextformat.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qtextoption.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qtgui-config.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qtguiglobal.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qtransform.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qvalidator.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qwindowdefs.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtGui/qwindowdefs_win.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QComboBox \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QGroupBox \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QHBoxLayout \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QInputDialog \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QLabel \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QLayout \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QLineEdit \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QMessageBox \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QPushButton \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QScrollArea \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QTextEdit \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QVBoxLayout \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/QWidget \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qabstractbutton.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qabstractitemdelegate.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qabstractscrollarea.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qabstractslider.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qabstractspinbox.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qboxlayout.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qcombobox.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qdialog.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qframe.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qgridlayout.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qgroupbox.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qinputdialog.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qlabel.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qlayout.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qlayoutitem.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qlineedit.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qmessagebox.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qpushbutton.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qrubberband.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qscrollarea.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qsizepolicy.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qslider.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qstyle.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qstyleoption.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qtabbar.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qtabwidget.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qtextedit.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qtwidgets-config.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qtwidgetsglobal.h \
+  C:/Users/hima/miniconda3/Library/include/qt/QtWidgets/qwidget.h \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5/Qt5Config.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5/Qt5ConfigVersion.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5/Qt5ModuleLocation.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Core/Qt5CoreConfig.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Core/Qt5CoreConfigExtras.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Core/Qt5CoreConfigExtrasMkspecDir.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Core/Qt5CoreConfigVersion.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Core/Qt5CoreMacros.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5GuiConfig.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5GuiConfigVersion.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QGifPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QICNSPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QICOPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QJpegPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QMinimalIntegrationPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QOffscreenIntegrationPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QPdfPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QSvgIconPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QSvgPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QTgaPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QTiffPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QTuioTouchPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QVirtualKeyboardPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QWbmpPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QWebGLIntegrationPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QWebpPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QWindowsDirect2DIntegrationPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QWindowsIntegrationPlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Gui/Qt5Gui_QXdgDesktopPortalThemePlugin.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Widgets/Qt5WidgetsConfig.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Widgets/Qt5WidgetsConfigExtras.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Widgets/Qt5WidgetsConfigVersion.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Widgets/Qt5WidgetsMacros.cmake \
+  C:/Users/hima/miniconda3/Library/lib/cmake/Qt5Widgets/Qt5Widgets_QWindowsVistaStylePlugin.cmake \
+  K:/C++/GUI\ Qt/NewsGUI/CMakeLists.txt \
+  K:/C++/GUI\ Qt/NewsGUI/adminwidget.cpp \
+  K:/C++/GUI\ Qt/NewsGUI/adminwidget.h \
+  K:/C++/GUI\ Qt/NewsGUI/loadandstore.cpp \
+  K:/C++/GUI\ Qt/NewsGUI/loadandstore.h \
+  K:/C++/GUI\ Qt/NewsGUI/loginpage.cpp \
+  K:/C++/GUI\ Qt/NewsGUI/loginpage.h \
+  K:/C++/GUI\ Qt/NewsGUI/main.cpp \
+  CMakeFiles/3.26.0-msvc3/CMakeCXXCompiler.cmake \
+  CMakeFiles/3.26.0-msvc3/CMakeRCCompiler.cmake \
+  CMakeFiles/3.26.0-msvc3/CMakeSystem.cmake \
+  K:/C++/GUI\ Qt/NewsGUI/registerpage.cpp \
+  K:/C++/GUI\ Qt/NewsGUI/registerpage.h \
+  K:/C++/GUI\ Qt/NewsGUI/sortedlinkedlist.cpp \
+  K:/C++/GUI\ Qt/NewsGUI/sortedlinkedlist.h \
+  K:/C++/GUI\ Qt/NewsGUI/userWidget.cpp \
+  K:/C++/GUI\ Qt/NewsGUI/userWidget.h \
+  K:/C++/GUI\ Qt/NewsGUI/widget.ui \
+  C:/Program\ Files/Microsoft\ Visual\ Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe
